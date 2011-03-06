3793a89f8df70c957e3a5e965361a0630f0e81ac
Brian Sniffen <bsniffen@akamai.com>
2011-03-06 17:57:22 -0500

    build universal readline

--- Library/Formula/readline.rb
+++ Library/Formula/readline.rb
@@ -26,7 +26,8 @@ EOS
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--infodir=#{info}",
+			  "--disable-dependency-tracking",
                           "--enable-multibyte"
-    system "make install"
+    system "make install CFLAGS='-arch ppc -arch i386 -arch x86_64' LDFLAGS=' -arch ppc -arch i386 -arch x86_64'"
   end
 end
