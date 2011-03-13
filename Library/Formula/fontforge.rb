20db04971f157eb0560311254f72a636c839a74b
Adam Vandenberg <flangy@gmail.com>
2011-03-13 11:13:53 -0700

    fontforge - fix linking w/ gettext 0.18

--- Library/Formula/fontforge.rb
+++ Library/Formula/fontforge.rb
@@ -12,8 +12,11 @@ class Fontforge < Formula
   depends_on 'libpng'
 
   def install
-    ENV.x11
     fails_with_llvm "Compiling cvexportdlg.c fails with error: initializer element is not constant"
+
+    ENV.x11
+    # Fix linker error; see: http://trac.macports.org/ticket/25012
+    ENV.append "LDFLAGS", "-lintl"
     system "./configure", "--prefix=#{prefix}",
                           "--enable-double",
                           "--without-freetype-bytecode",
