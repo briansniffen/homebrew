ca9e22e5c6a05cc3a631286fa604bcf7353d4c9c
Brian Sniffen <bsniffen@akamai.com>
2011-03-21 12:28:33 -0400

    avoid kqueue for dovecot

--- Library/Formula/dovecot.rb
+++ Library/Formula/dovecot.rb
@@ -13,6 +13,7 @@ class Dovecot < Formula
                           "--libexecdir=#{libexec}",
                           "--sysconfdir=#{etc}",
                           "--localstatedir=#{var}",
+                          "--with-ioloop=poll",
                           "--with-ssl=openssl"
     system "make install"
   end
